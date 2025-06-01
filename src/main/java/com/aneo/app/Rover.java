bb818451b427bad8c0e48f997c2cda1a6c82513e
FernandoVR <fvr.dev@gmail.com>
2025-06-01 23:54:29 +0000

    feat: follow instructions

--- src/main/java/com/aneo/app/Rover.java
+++ src/main/java/com/aneo/app/Rover.java
@@ -7,6 +7,9 @@ public class Rover {
     private Plateau plateau;
 
     public Rover(int x, int y, char direction, Plateau plateau) {
+        if (x < 0 || y < 0 || x > plateau.maxX() || y > plateau.maxY()) {
+            throw new IllegalArgumentException("Rover position out of bounds");
+        }
         this.x = x;
         this.y = y;
         this.direction = direction;
@@ -15,6 +18,65 @@ public class Rover {
 
     public String getPosition() {
         return x + " " + y + " " + direction;
+    }
+
+    public void followInstructions(String instructions) {
+        for (char instruction : instructions.toCharArray()) {
+            switch (instruction) {
+                case 'L' -> direction = turnLeft(direction);
+                case 'R' -> direction = turnRight(direction);
+                case 'M' -> {
+                    moveForward();
+                }
+                default -> throw new IllegalArgumentException("Invalid instruction: " + instruction);
+            }
+        }
+    }
+
+    private char turnLeft(char currentDirection) {
+        return switch (currentDirection) {
+            case 'N' -> 'W';
+            case 'W' -> 'S';
+            case 'S' -> 'E';
+            case 'E' -> 'N';
+            default -> throw new IllegalArgumentException("Invalid direction: " + currentDirection);
+        };
+    }
+
+    private char turnRight(char currentDirection) {
+        return switch (currentDirection) {
+            case 'N' -> 'E';
+            case 'E' -> 'S';
+            case 'S' -> 'W';
+            case 'W' -> 'N';
+            default -> throw new IllegalArgumentException("Invalid direction: " + currentDirection);
+        };
+    }
+
+    private void moveForward() {
+        switch (direction) {
+            case 'N' -> {
+                if (y < plateau.maxY()) {
+                    y++;
+                }
+            }
+            case 'E' -> {
+                if (x < plateau.maxX()) {
+                    x++;
+                }
+            }
+            case 'S' -> {
+                if (y > 0) {
+                    y--;
+                }
+            }
+            case 'W' -> {
+                if (x > 0) {
+                    x--;
+                }
+            }
+            default -> throw new IllegalArgumentException("Invalid direction: " + direction);
+        }
     }
 
 }
